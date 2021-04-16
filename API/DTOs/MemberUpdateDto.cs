70c8abab5c613fcee24b7d5100c4935cbbedc540
AzureAD\ChrisDorman <chris.dorman@allsop.software>
2021-04-16 08:38:04 +0100

    End of Section 10

---
+++ API/DTOs/MemberUpdateDto.cs
@@ -0,0 +1,11 @@
+namespace API.DTOs
+{
+    public class MemberUpdateDto
+    {
+        public string Intoduction{ get; set;}
+        public string LookingFor{ get; set;}
+        public string Interests{ get; set;}
+        public string City{ get; set;}
+        public string Country{ get; set;}
+    }
+}
